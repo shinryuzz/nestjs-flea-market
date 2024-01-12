1c47b1cb55f5ccee1d4babcfd105a1ae4629db2c
Shinryuzz <mshin1127@gmail.com>
2024-01-12 11:53:36 +0900

    refactor: simplify create() method with DTO

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -8,8 +8,8 @@ import {
   Post,
 } from '@nestjs/common';
 import { ItemsService } from './items.service';
-import { ItemStatus } from './item-status.enum';
 import { Item } from './item.model';
+import { CreateItemDto } from './dto/create-item.dto';
 
 @Controller('items')
 export class ItemsController {
@@ -25,21 +25,8 @@ export class ItemsController {
   }
 
   @Post()
-  create(
-    @Body('id') id: string,
-    @Body('name') name: string,
-    @Body('price') price: number,
-    @Body('description') description: string,
-  ): Item {
-    const item: Item = {
-      id,
-      name,
-      price,
-      description,
-      status: ItemStatus.ON_SALE,
-    };
-
-    return this.itemsService.create(item);
+  create(@Body() createItemDto: CreateItemDto): Item {
+    return this.itemsService.create(createItemDto);
   }
 
   @Patch(':id')
